9e01f6e498cc2c9188540483bd019b8ac74fbc42
eerne <jokey13@bk.ru>
2019-08-27 01:34:42 +0300

    Added some 3d models, that I am going to delete

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,16 +2,12 @@
 
 public class FollowPlayer : MonoBehaviour
 {
-    public float animationSpeed = 3f;
-    public float magnitude = 0.1f;
 
     private Transform player;
-    private float start_y;
-    private float movement;
+    
 
     void Start()
     {
-        start_y = transform.position.y;
         player = GameObject.FindWithTag("Player").transform;
     }
 
